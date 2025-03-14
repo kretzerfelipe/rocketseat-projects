214fd18129153ab389c8ad2f9499bc6bd460c0e4
kretzerfelipe <felipegk2006@gmail.com>
2025-03-14 11:06:42 -0300

    ✨ feat: finalizada validação de formulário

--- react/02-aprofundamento-react/src/pages/home/index.tsx
+++ react/02-aprofundamento-react/src/pages/home/index.tsx
@@ -1,25 +1,64 @@
 import { Play } from "phosphor-react";
-import { ContdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, TaskInput } from "./styles";
+import {
+	ContdownContainer,
+	FormContainer,
+	HomeContainer,
+	MinutesAmountInput,
+	Separator,
+	TaskInput,
+} from "./styles";
 import { Button } from "../../components/button/styles";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface NewCicleFormData {
+	task: string;
+	minutesAmount: number;
+}
 
 export default function Home() {
-	const [task, setTask] = useState("");
-	const [minutesAmnount, setMinutesAmount] = useState(0)
+	const { register, handleSubmit, watch, reset } = useForm<NewCicleFormData>({
+		defaultValues: {
+			task: '',
+			minutesAmount: 5
+		}
+	});
+
+	function handleCreateNewCicle(data: NewCicleFormData) {
+		console.log(data)
+		reset()
+	}
+
+	const task = watch('task')
+	const minutesAmount = watch('minutesAmount')
+	const isSubmitDisabled = !task || minutesAmount > 60 || minutesAmount < 5
 
 	return (
 		<HomeContainer>
-			<form>
+			<form onSubmit={handleSubmit(handleCreateNewCicle)}>
 				<FormContainer>
 					<label htmlFor='task'>Vou trabalhar em</label>
-					<TaskInput type='text' onChange={(e) => setTask(e.target.value)} value={task} list='task-suggestions' id='task' placeholder='Dê um nome para seu projeto' />
+					<TaskInput
+						type='text'
+						list='task-suggestions'
+						id='task'
+						placeholder='Dê um nome para seu projeto'
+						{...register("task")}
+					/>
 
 					<datalist id='task-suggestions'>
 						<option value='projeto01'></option>
 					</datalist>
 
 					<label htmlFor='minutesAmount'>durante</label>
-					<MinutesAmountInput onChange={(e) => setMinutesAmount(Number(e.target.value))} step={5} type='number' id='minutesAmount' placeholder='00' min={5} max={60} />
+					<MinutesAmountInput
+						step={5}
+						type='number'
+						id='minutesAmount'
+						placeholder='00'
+						min={5}
+						max={60}
+						{...register("minutesAmount", {valueAsNumber: true})}
+					/>
 
 					<span>minutos.</span>
 				</FormContainer>
@@ -32,7 +71,7 @@ export default function Home() {
 					<span>0</span>
 				</ContdownContainer>
 
-				<Button type='submit'>
+				<Button disabled={isSubmitDisabled} type='submit'>
 					<Play />
 					Começar
 				</Button>
